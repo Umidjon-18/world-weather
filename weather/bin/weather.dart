dcb87d446204d66f32cebc853b8111fe0501ee4a
Umidjon-18 <umidjonxomidjonovich@gmail.com>
2022-06-01 06:57:47 +0500

    Finished Inshaa Alloh

---
+++ weather/bin/weather.dart
@@ -0,0 +1,13 @@
+import 'package:hive/hive.dart';
+import 'app.dart';
+import 'utils.dart';
+import 'weather_model.dart';
+
+Utils utils = Utils();
+
+void main(List<String> arguments) async {
+  Hive.init("../src");
+  Hive.registerAdapter<WeatherModel>(WeatherModelAdapter());
+  App().weatherGo();
+  
+}
